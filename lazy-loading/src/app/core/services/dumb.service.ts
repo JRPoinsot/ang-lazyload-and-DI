3a9bc121cac2e515c111d64ba2fe307b05020807
Jean-Rodolphe POINSOT <jr.poinsot@gmail.com>
2020-11-25 18:58:17 +0100

    Add service id

--- lazy-loading/src/app/core/services/dumb.service.ts
+++ lazy-loading/src/app/core/services/dumb.service.ts
@@ -8,16 +8,19 @@ export class DumbService implements OnDestroy {
 
   private countSubject = new BehaviorSubject<number>(0);
   public count$ = this.countSubject.asObservable();
+  public serviceid = Math.round(Math.random() * 10000);
 
   constructor() {
-    console.log('Service Created !');
+    console.log('Service Created ', this.serviceid);
   }
 
   ngOnDestroy(): void {
-    console.log('Service Destroyed !')
+    console.log('Service Destroyed ', this.serviceid);
   }
 
   public increment(): void {
     this.countSubject.next(this.countSubject.value + 1);
   }
 }
+
+
